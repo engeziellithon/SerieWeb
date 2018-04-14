9b910e32c19782dbecafb0a693d0a55043d1a4db
Engeziellithon <zievs32@yahoo.com.br>
2018-04-14 14:31:55 -0300

    Pesquisa na index

--- SerieWeb/Controllers/Site/HomeController.cs
+++ SerieWeb/Controllers/Site/HomeController.cs
@@ -1,15 +1,32 @@
-﻿using System.Web.Mvc;
+﻿using SerieWeb.Models.Identity;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace SerieWeb.Controllers
 {
     [RequireHttps]
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
-        
+
+        [HttpGet]
+        public ActionResult Pesquisa()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Pesquisa(string pesquisa)
+        {
+            return View(db.Series.Where(x => x.NomeSerie.Contains(pesquisa)).OrderBy(x => x.NomeSerie));
+        }
+
     }
 }
