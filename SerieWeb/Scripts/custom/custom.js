c522ca7c57b8c7f884f2680a798283a88c291d96
Gabriel Silva Lana <gsilva902@gmail.com>
2018-04-14 11:47:47 -0300

    pagina unica para comentarios do facebook

---
+++ SerieWeb/Scripts/custom/custom.js
@@ -0,0 +1,39 @@
+﻿$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+//#to-top button appears after scrolling
+var fixed = false;
+$(document).scroll(function () {
+    if ($(this).scrollTop() > 250) {
+        if (!fixed) {
+            fixed = true;
+            // $('#to-top').css({position:'fixed', display:'block'});
+            $('#to-top').show("slow", function () {
+                $('#to-top').css({
+                    position: 'fixed',
+                    display: 'block'
+                });
+            });
+        }
+    } else {
+        if (fixed) {
+            fixed = false;
+            $('#to-top').hide("slow", function () {
+                $('#to-top').css({
+                    display: 'none'
+                });
+            });
+        }
+    }
+});
