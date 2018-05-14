a39e2bfd8362a5c3fb1fc5b214763561970be0ae
Engeziellithon <zievs32@yahoo.com.br>
2018-05-14 18:42:24 -0300

    Usuario autorizacao and perfil

--- SerieWeb/Migrations/201803222246187_TableUserSerieTemporadaEpisodioGenero.cs
+++ SerieWeb/Migrations/201803222246187_TableUserSerieTemporadaEpisodioGenero.cs
@@ -9,124 +9,124 @@ namespace SerieWeb.Migrations
             CreateTable(
                 "dbo.Episodio",
                 c => new
-                    {
-                        EpisodioID = c.Int(nullable: false, identity: true),
-                        NomeEpisodio = c.String(nullable: false, maxLength: 30),
-                        Sinopse = c.String(maxLength: 255),
-                        Video = c.String(maxLength: 255),
-                        DataExibicao = c.DateTime(nullable: false),
-                        TemporadaID = c.Int(nullable: false),
-                    })
+                {
+                    EpisodioID = c.Int(nullable: false, identity: true),
+                    NomeEpisodio = c.String(nullable: false, maxLength: 30),
+                    Sinopse = c.String(maxLength: 255),
+                    Video = c.String(maxLength: 255),
+                    DataExibicao = c.DateTime(nullable: false),
+                    TemporadaID = c.Int(nullable: false),
+                })
                 .PrimaryKey(t => t.EpisodioID)
                 .ForeignKey("dbo.Temporada", t => t.TemporadaID, cascadeDelete: true)
                 .Index(t => t.TemporadaID);
-            
+
             CreateTable(
                 "dbo.Temporada",
                 c => new
-                    {
-                        TemporadaID = c.Int(nullable: false, identity: true),
-                        NomeTemporada = c.String(nullable: false, maxLength: 30),
-                        SerieID = c.Int(nullable: false),
-                    })
+                {
+                    TemporadaID = c.Int(nullable: false, identity: true),
+                    NomeTemporada = c.String(nullable: false, maxLength: 30),
+                    SerieID = c.Int(nullable: false),
+                })
                 .PrimaryKey(t => t.TemporadaID)
                 .ForeignKey("dbo.Serie", t => t.SerieID, cascadeDelete: true)
                 .Index(t => t.SerieID);
-            
+
             CreateTable(
                 "dbo.Serie",
                 c => new
-                    {
-                        SerieID = c.Int(nullable: false, identity: true),
-                        NomeSerie = c.String(nullable: false, maxLength: 30),
-                        Imagem = c.String(),
-                        Sinopse = c.String(nullable: false, maxLength: 256),
-                        Nota = c.Double(nullable: false),
-                        Genero_GeneroID = c.Int(),
-                    })
+                {
+                    SerieID = c.Int(nullable: false, identity: true),
+                    NomeSerie = c.String(nullable: false, maxLength: 30),
+                    Imagem = c.String(),
+                    Sinopse = c.String(nullable: false, maxLength: 256),
+                    Nota = c.Double(nullable: false),
+                    Genero_GeneroID = c.Int(),
+                })
                 .PrimaryKey(t => t.SerieID)
                 .ForeignKey("dbo.Generoes", t => t.Genero_GeneroID)
                 .Index(t => t.Genero_GeneroID);
-            
+
             CreateTable(
                 "dbo.Generoes",
                 c => new
-                    {
-                        GeneroID = c.Int(nullable: false, identity: true),
-                        NomeGenero = c.String(nullable: false, maxLength: 100),
-                    })
+                {
+                    GeneroID = c.Int(nullable: false, identity: true),
+                    NomeGenero = c.String(nullable: false, maxLength: 100),
+                })
                 .PrimaryKey(t => t.GeneroID);
-            
+
             CreateTable(
                 "dbo.AspNetRoles",
                 c => new
-                    {
-                        Id = c.String(nullable: false, maxLength: 128),
-                        Name = c.String(nullable: false, maxLength: 256),
-                    })
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    Name = c.String(nullable: false, maxLength: 256),
+                })
                 .PrimaryKey(t => t.Id)
                 .Index(t => t.Name, unique: true, name: "RoleNameIndex");
-            
+
             CreateTable(
                 "dbo.AspNetUserRoles",
                 c => new
-                    {
-                        UserId = c.String(nullable: false, maxLength: 128),
-                        RoleId = c.String(nullable: false, maxLength: 128),
-                    })
+                {
+                    UserId = c.String(nullable: false, maxLength: 128),
+                    RoleId = c.String(nullable: false, maxLength: 128),
+                })
                 .PrimaryKey(t => new { t.UserId, t.RoleId })
                 .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
                 .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
                 .Index(t => t.UserId)
                 .Index(t => t.RoleId);
-            
+
             CreateTable(
                 "dbo.AspNetUsers",
                 c => new
-                    {
-                        Id = c.String(nullable: false, maxLength: 128),
-                        Email = c.String(maxLength: 256),
-                        EmailConfirmed = c.Boolean(nullable: false),
-                        PasswordHash = c.String(),
-                        SecurityStamp = c.String(),
-                        PhoneNumber = c.String(),
-                        PhoneNumberConfirmed = c.Boolean(nullable: false),
-                        TwoFactorEnabled = c.Boolean(nullable: false),
-                        LockoutEndDateUtc = c.DateTime(),
-                        LockoutEnabled = c.Boolean(nullable: false),
-                        AccessFailedCount = c.Int(nullable: false),
-                        UserName = c.String(nullable: false, maxLength: 256),
-                    })
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    Email = c.String(maxLength: 256),
+                    EmailConfirmed = c.Boolean(nullable: false),
+                    PasswordHash = c.String(),
+                    SecurityStamp = c.String(),
+                    PhoneNumber = c.String(),
+                    PhoneNumberConfirmed = c.Boolean(nullable: false),
+                    TwoFactorEnabled = c.Boolean(nullable: false),
+                    LockoutEndDateUtc = c.DateTime(),
+                    LockoutEnabled = c.Boolean(nullable: false),
+                    AccessFailedCount = c.Int(nullable: false),
+                    UserName = c.String(nullable: false, maxLength: 256),
+                })
                 .PrimaryKey(t => t.Id)
                 .Index(t => t.UserName, unique: true, name: "UserNameIndex");
-            
+
             CreateTable(
                 "dbo.AspNetUserClaims",
                 c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        UserId = c.String(nullable: false, maxLength: 128),
-                        ClaimType = c.String(),
-                        ClaimValue = c.String(),
-                    })
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    UserId = c.String(nullable: false, maxLength: 128),
+                    ClaimType = c.String(),
+                    ClaimValue = c.String(),
+                })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
                 .Index(t => t.UserId);
-            
+
             CreateTable(
                 "dbo.AspNetUserLogins",
                 c => new
-                    {
-                        LoginProvider = c.String(nullable: false, maxLength: 128),
-                        ProviderKey = c.String(nullable: false, maxLength: 128),
-                        UserId = c.String(nullable: false, maxLength: 128),
-                    })
+                {
+                    LoginProvider = c.String(nullable: false, maxLength: 128),
+                    ProviderKey = c.String(nullable: false, maxLength: 128),
+                    UserId = c.String(nullable: false, maxLength: 128),
+                })
                 .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
                 .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
                 .Index(t => t.UserId);
-            
+
         }
-        
+
         public override void Down()
         {
             DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
